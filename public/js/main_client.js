a97efda22ae6079a89bc7a21ee9c76e5cb25a4f1
Scott Hill <scott289@gmail.com>
2016-01-30 14:22:07 -0800

    bounce effect

--- public/js/main_client.js
+++ public/js/main_client.js
@@ -3,14 +3,9 @@ $(document).ready(function() {
   $("#btnFortune").click(function(event){
     $.getJSON('/api/fortune', function(fortune) {
       $('#fortuneDiv').html(fortune.fortune);
+      $("#fortuneDiv").effect( "bounce", "slow" );
     });
   });
-
-  $(".nav a").on("click", function(){
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-  });
-
 
   /* Set the active tag for current navbar link. */
   $('.nav a[href="' + this.location.pathname + '"]').parent().addClass('active');
