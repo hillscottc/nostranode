da8ff9587e82d907e7836e2a3de013ffb90fb57c
Scott Hill <scott289@gmail.com>
2016-05-04 10:03:00 -0700

    unfinished

---
+++ public/js/App.js
@@ -0,0 +1,51 @@
+'use strict';
+
+import React from 'react';
+import { render } from 'react-dom';
+import CountryList from './CountryList';
+import $ from 'jquery';
+
+const App = React.createClass({
+
+
+  loadFromApi: function() {
+    // const url = "http://disneypubworlddev.cp.disney.com/demo2/public/api.asp?countries=1";
+    // $.ajax({
+    //   url: url,
+    //   dataType: 'json',
+    //   cache: false,
+    //   success: function(data) {
+    //     this.setState({countries: data});
+    //   }.bind(this),
+    //   error: function(xhr, status, err) {
+    //     console.error(url, status, err.toString());
+    //   }.bind(this)
+    // });
+  },
+
+
+  getInitialState: function () {
+    // return {"countries": [{"Country":"USA", "ListCode":"1"}, {"Country":"Sweden", "ListCode": "2"}]}
+    return {"countries": []}
+  },
+
+  componentDidMount: function() {
+    // this.loadFromApi();
+  },
+
+  render: function() {
+
+    return (
+        <div>
+          Hello, world!
+          <CountryList countries={this.state.countries}/>
+        </div>
+    );
+  }
+});
+
+
+export default App;
+
+
+
